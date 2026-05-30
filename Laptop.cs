5302a8a8d560a3b09818a112ff389853d01f2069
Eric Mokuma <mokumaeric@gmail.com>
2026-05-30 16:36:17 +1200

    Laptop clas completed

--- Laptop.cs
+++ Laptop.cs
@@ -1,6 +1,25 @@
-namespace Ele ShoppingApp;
+namespace Ele_ShoppingApp;
 
-public class Laptop
+public class Laptop: Products
 {
-    
+    // List Fiels
+    private int laptopRam;
+    private int storage;
+    private string processor;
+    private double size;
+
+    // List Properties
+    public int LaptopRAM { get { return laptopRam; } set { laptopRam = value; } }
+    public int Storage { get { return storage; } set { storage = value; } }
+    public string Processor { get { return processor; } set { processor = value; } }
+    public double Size { get { return size; } set { size = value; } }
+
+    // List Constructor
+    public Laptop(int laptopRam, int storage, string processor, double size)
+    {
+        LaptopRAM = laptopRam;
+        Storage = storage;
+        Processor = processor;
+        Size = size;
+    }
 }
